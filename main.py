f249b181d556b480167da90b0cfc961f8ca18fac
cristalixmodded <120457846+cristalixmodded@users.noreply.github.com>
2022-12-17 14:59:39 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,99 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QIntValidator, QIcon
+from PyQt6.QtCore import QTimer
+from PyQt6.QtTest import QTest
+from PyQt6 import uic
+import configparser
+import threading
+import send
+
+class Ui(QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('MainWindow.ui', self)
+        self.show()
+        
+        self.setWindowIcon(QIcon('img\cattocry.png'))
+        
+        self.channelID.setValidator(QIntValidator(self))
+        self.delay.setValidator(QIntValidator(self))
+        self.randomDelayMIN.setValidator(QIntValidator(self))
+        self.randomDelayMAX.setValidator(QIntValidator(self))
+
+        self.startButton.clicked.connect(self.start_button)
+        self.randomTimeCheckBox.toggled.connect(self.check)
+        
+        self.load_config()
+        
+    def check(self):    
+        if self.randomTimeCheckBox.isChecked():
+            self.delay.setEnabled(False)
+            self.randomDelayMIN.setEnabled(True)
+            self.randomDelayMAX.setEnabled(True)
+            
+            self.delay.setText('')
+        else:
+            self.delay.setEnabled(True)
+            self.randomDelayMIN.setEnabled(False)
+            self.randomDelayMAX.setEnabled(False)
+            
+            self.randomDelayMIN.setText('')
+            self.randomDelayMAX.setText('')
+        
+    def load_config(self):
+        config = configparser.ConfigParser()
+        config.read_file(open(r'config.ini'))
+        discord_token = config.get('Config', 'discord_token')
+        warp_name = config.get('Config', 'warp_name')
+        delay = config.get('Config', 'delay')
+        MIN_delay = config.get('Config', 'MIN_delay')
+        MAX_delay = config.get('Config', 'MAX_delay')
+        channel_ID = config.get('Config', 'channel_ID')
+        message_content = config.get('Config', 'message_content')
+        
+        delete_message = eval(config.get('Config', 'delete_message'))
+        random_time = eval(config.get('Config', 'random_time'))
+            
+        self.discordToken.setText(discord_token)
+        self.warpName.setText(warp_name)
+        self.delay.setText(delay)
+        self.randomDelayMIN.setText(MIN_delay)
+        self.randomDelayMAX.setText(MAX_delay)
+        self.channelID.setText(channel_ID)
+        self.messageContent.setText(message_content)
+        
+        self.deleteMessageCheckBox.setChecked(delete_message)
+        self.randomTimeCheckBox.setChecked(random_time)
+    
+    def save_config(self):
+        config = configparser.ConfigParser()
+        config.add_section('Config')
+        config.set('Config', 'discord_token', self.discordToken.text())
+        config.set('Config', 'warp_name', self.warpName.text())
+        config.set('Config', 'delay', self.delay.text())
+        config.set('Config', 'MIN_delay', self.randomDelayMIN.text())
+        config.set('Config', 'MAX_delay', self.randomDelayMAX.text())
+        config.set('Config', 'channel_ID', self.channelID.text())
+        config.set('Config', 'message_content', self.messageContent.toPlainText())
+            
+        config.set('Config', 'delete_message', str(self.deleteMessageCheckBox.isChecked()))
+        config.set('Config', 'random_time', str(self.randomTimeCheckBox.isChecked()))
+    
+        with open('config.ini', 'w') as config_file:
+            config.write(config_file)
+        
+    def start_button(self):
+        self.save_config()      
+        self.startButton.setEnabled(False)
+        self.startButton.setText('by matswuuu')
+
+        send_thread = threading.Thread(target=send.send, name='send_thread')
+        if not send_thread.is_alive():
+            send_thread.start()
+
+    def closeEvent(self, event):
+        self.save_config()
+            
+app = QApplication([])
+UIWindow = Ui()
+app.exec()
---
+++ send.py
@@ -0,0 +1,78 @@
+import json
+import discum
+import time
+from datetime import datetime, timedelta
+import pytz
+import random
+import configparser
+
+config = configparser.ConfigParser()
+config.read_file(open(r'config.ini'))
+discord_token = config.get('Config', 'discord_token')
+warp_name = config.get('Config', 'warp_name')
+delay = config.get('Config', 'delay') * 60
+MIN_delay = config.get('Config', 'MIN_delay') * 60
+MAX_delay = config.get('Config', 'MAX_delay') * 60
+channel_ID = config.get('Config', 'channel_ID')
+message_content = config.get('Config', 'message_content')
+
+delete_message = eval(config.get('Config', 'delete_message'))
+random_time = eval(config.get('Config', 'random_time'))
+
+bot = discum.Client(token=discord_token)
+
+def send():
+    last_message_id = None
+    while True:
+        messages = bot.getMessages(channelID=channel_ID, num=100, beforeDate=last_message_id)
+        messages = messages.text
+        messages = messages.replace('\\n', '')
+        messages = messages.replace('\\"', '')
+        messages = messages.encode().decode("unicode-escape")
+        messages = messages.replace('\\', '')
+
+        messages = json.loads(messages)
+
+        message_founded = False
+        for j in messages:
+            if not message_founded:
+                if j['author'].get('id') == '618536577282342912':
+                    content = j['content']
+                    for wn in warp_name:
+                        if ' '+wn in content:
+                            pos = content.find(' '+wn)
+                            if pos+len(wn)+1 < len(content):
+                                if not content[pos+len(wn)+1].isalnum():
+                                    message_founded = True
+                                    message_timestamp = j['content'][1:20]
+                            else:
+                                message_founded = True
+                                message_timestamp = j['content'][1:20]
+            last_message_id = j['id']
+
+        if message_founded:
+            message_founded = False
+            last_message_id = None
+
+            message_time = datetime.strptime(message_timestamp, '%d.%m.%Y %H:%M:%S')
+            now = datetime.now() - timedelta(hours=2)
+
+            if message_time < now - timedelta(minutes=10):
+                sended_message = bot.sendMessage(channelID=channel_ID, message=message_content)
+                sended_message = sended_message.text.encode().decode('unicode-escape')
+
+                if delete_message:
+                    sended_message_json = json.loads(sended_message.replace('\\', ''))
+                    bot.deleteMessage(channelID=channel_ID, messageID=sended_message_json['id'])
+                time.sleep(600)
+            else:
+                wait = message_time - now + timedelta(minutes=10)
+                time.sleep(wait.total_seconds())
+
+            if random_time:
+                time.sleep(random.randint(MIN_delay, MAX_delay))
+            else:
+                time.sleep(delay)
+
+        else:
+            time.sleep(1)
